214830dcc1b387dcfed59c9a13a0f580451b445c
mackitluck <mackitluck@gmail.com>
2024-09-24 13:53:20 -0500

    footer name changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer class="p-4 bg-skyblue rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 footer">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 footer-left-text">
-        Developed by Abhinav Kumar
+        Developed by Manish Patel
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 footer-contact">
         <li>
